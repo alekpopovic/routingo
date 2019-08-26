8f8db0f2b07477f14b0e273294ed9aee4ce696d4
Aleksandar Popovic <aleksandar.popovic.popac@gmail.com>
2019-08-26 17:27:51 +0200

    Setup fix

--- lib/generators/routingo/install_generator.rb
+++ lib/generators/routingo/install_generator.rb
@@ -8,8 +8,8 @@ module Routingo
     class Install < Rails::Generators::Base
       def insert_path
         klazz = 'class Application < Rails::Application'
-        paths = 'config/routes.rb'
-        joins = 'config/routes/*.rb'
+        paths = "'config/routes.rb'"
+        joins = "'config/routes/*.rb'"
         insert_into_file 'config/application.rb', after: "#{klazz}\n" do
           "config.paths['#{paths}'].concat(Dir[Rails.root.join('#{joins}')])\n"
         end
